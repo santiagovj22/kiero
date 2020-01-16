e2ea32849909a78488c85dc6db421b2f8d187cfb
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-16 12:29:18 -0500

    starting data send

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,56 +5,71 @@ import { Link, Redirect } from "react-router-dom";
 
 class Login extends React.Component{
    
-  //   constructor(props){
-  //     super(props)
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.handleInput = this.handleInput.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  //     this.state = {
-  //      email : '',
-  //     password : ''
-  //   }
-  //   this.handleInput = this.handleInput.bind(this);
-  //   this.handleSubmit = this.handleSubmit.bind(this);
-  //  }
-
-  //  handleInput(e) {
-  //    this.state[e.target.name] = e.target.value; 
-  //  }
+  handleInput(e) {
+    this.state[e.target.name] = e.target.value;
+  }
 
     render(){
         return(
             <React.Fragment>
                 <div className="main-login">
-                <h1 className ="h3 mb-3 font-weight-normal">Sign in</h1>
+                  <h1 className ="h3 mb-3 font-weight-normal">Sign in</h1>
                     <div className="ui_not-register">
                         ¿Eres nuevo en este sitio?
-                <Link to = "/register" className='txt-password'>  Regístrate</Link>
-            </div>
-            <div className="btn_login-user">
+                        <Link to = "/register" className='txt-password'>  Regístrate</Link>
+                    </div>
+              <div className="btn_login-user">
+                <div className="auth_social">
+                    {/* Login with google */}
+                </div> 
               <div className="auth_social">
-            {/* Login with google */}
-            </div> 
-            <div className="auth_social">
-               {/* Login with facebook  */}
-            </div>
+                    {/* Login with facebook  */}
+                </div>
             <div className="auth_social"><h2><span>o</span></h2></div>
                 <br/>
-            <div className='form-group'>
-              <label htmlFor = "email">Email</label>
-              <input type = "email" className = "form-control" name = "email" placeholder = "enter email"></input>
-            </div>
-            <div className='form-group'>
-              <label htmlFor = "password">Password</label>
-              <input type = "password" className = "form-control" name = "password" placeholder = "enter password"></input>
-            </div>
-            <div className="inp-forgetpassw">
-              <Link to = "/forgetPassword" className="txt-password">¿Olvidaste tu contraseña?</Link>
-            </div>
+                <form onSubmit = {this.handleSubmit}>
+                      <div className='form-group'>
+                      <label htmlFor = "email">Email</label>
+                      <input type = "email"
+                             className = "form-control"
+                             name = "email" 
+                             placeholder = "enter email"
+                             onChange = {this.handleInput}
+                             required>
+                       </input>
+                  </div>
+                  <div className='form-group'>
+                    <label htmlFor = "password">Password</label>
+                    <input type = "password"
+                           className = "form-control" 
+                           name = "password" 
+                           placeholder = "enter password"
+                           onChange = {this.handleInput}
+                           required>
+                    </input>
+                  </div>
+                  <div className="inp-forgetpassw">
+                    <Link to = "/forgetPassword" className="txt-password">¿Olvidaste tu contraseña?</Link>
+                  </div>
 
-            <div className="container_fpu-login">
-              <button onClick={this.handleSubmitUs} className="btn btn-login">
-                Ingresar
-              </button>
-            </div>
+                  <div className="container_fpu-login">
+                    <button  type = "submit" className="btn btn-login">
+                      Ingresar
+                    </button>
+                  </div>
+                </form>
+           
             </div>
           </div> 
             </React.Fragment>
