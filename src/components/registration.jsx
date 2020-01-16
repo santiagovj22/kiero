2b87e62210b0adf774344518d3f1636c0be395ae
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-16 12:56:20 -0500

    template register

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,65 +1,75 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 class Registration extends React.Component{
-    
+   
     render(){
         return(
             <React.Fragment>
               <div>    
                   <form >
                     <div className="main-login">
-                      <h1>Crear Cuenta</h1>
+                    <h1 className = "h3 mb-3 font-weight-normal">Sign up</h1>
                           <div className="auth_social"></div>
                       <div className="auth_social">
                       </div>
-                      <div className="first_name lg-name">
-                        <label htmlFor="first_name" className='label-login'>Nombre</label>
+                    <form>
+                      <div className="form-group">
+                        <label htmlFor="first_name">Nombre</label>
                         <input
                           type="text"
+                          className="form-control"
                           name="first_name"
+                          placeholder = "Ingresa tu Nombre"
                           size="50px"
                           required
                         />
                       </div>
-                      <div className="first_name">
-                        <label htmlFor="last_name" className='label-login'>Apellido</label>
+                      <div className="form-group">
+                        <label htmlFor="last_name">Apellido</label>
                         <input
                           type="text"
-                          name="lastName"
+                          className = "form-control"
+                          name="last_name"
+                          placeholder = "Ingresa tu apellido"
                           required
                         />
                       </div>
-                      <div className="email">
-                        <label htmlFor="email" className='label-login'>Correo</label>
+                      <div className="form-group">
+                        <label htmlFor="email">Correo</label>
                         <input
                           type="email"
+                          className = "form-control"
                           name="email"
+                          placeholder = "Ingresa tu correo"
                           required
                         />
                       
                       </div>
-                      <div className="password">
-                        <label htmlFor="password" className='label-login'>Contraseña</label>
+                      <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
                         <input
                           type="password"
+                          className = "form-control"
                           name="password"
+                          placeholder = "Ingresa tu contrasena"
                           required
                         />
                       </div>
 
-                      <div className = "confirmPass">
-                        <label htmlFor = "confirmPass" className = 'label-login'>Confirmar Contraseña</label>
-                        <input type = "password" name = "confirmPass" required />
+                      <div className = "form-group">
+                        <label htmlFor = "confirmPass">Confirmar Contraseña</label>
+                        <input 
+                         type = "password"
+                         name = "confirmPass"
+                         className = "form-control" 
+                         placeholder = "confirma contrasena"
+                         required />
                       </div>
 
-                      <div className="info">
-                        <small></small>
-                      </div>
                       <div className="text-center">
                         Creando una cuenta, usted ha aceptado los{" "}
                         <a
                           href="https://www.kiero.co/terms.html"
-                          
                         >
                           Términos y condiciones
                         </a>{" "}
@@ -73,14 +83,14 @@ class Registration extends React.Component{
                       </div>
                       <div style={{ marginTop: "40px", paddingBottom: "30px"}}>
                         <div className="container_fpu container ">
-                          <button className="btn btn-register ">
-                            
+                          <button  type = "submit" className="btn btn-register">
                             Crea tu cuenta en Kiero
                           </button>
                         </div>
-                        <div className="btn_login-user">
+                       
                       </div>
-                      </div>
+                      
+                      </form>
                     </div>
                   </form>
                 </div>
