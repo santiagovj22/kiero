0e0017ab1e3343106cf9f27035f67be20a3ad724
pechuga22 <iefaz.santiagovalle@gmail.com>
2020-01-16 15:23:20 -0500

    auth service ready

--- src/services/userAuthService.js
+++ src/services/userAuthService.js
@@ -1,4 +1,5 @@
 function userAuth(){
+
     let userIsAuth = false;
     let userId = "";
     let userData = "";
@@ -33,5 +34,41 @@ function userAuth(){
             err => console.error(err)
         }
     };
+
+    this.register = async function(user) {
+        try {
+            let response = await fetch(`${mainUrl}/users`,
+            {
+                method : "POST",
+                body : JSON.stringify(user),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+
+            }
+            );
+            let data = await response.json();
+            if(response.status === 400) return { error: true, message: data.error}
+            return{ error: false, id: data}
+            
+        } catch (error) {
+            console.error(error);
+        }
     
+    }
+    this.setHardAuth = () => {
+        userIsAuth = true
+    }
+    this.setId = function(id) {
+        userId = id;
+    }
+    this.getId = function () {
+        return userId;
+    }
+    this.logout = function () {
+        userIsAuth = false;
+    }
 }
+
+
+export default new userAuth();
